d7bb5bf3446423b9da223ad463c1b4fb430403a3
571559 <31239471+andersmh@users.noreply.github.com>
2018-11-08 22:04:00 +0100

    button fix

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,11 +7,16 @@ class Counter extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
